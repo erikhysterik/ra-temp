e44c9a9a0f4066fd5a562a340d1d4f3e9bbef22b
Erik Sautter <erikhysterik@users.noreply.github.com>
2021-12-01 00:21:38 -0600

    Page updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,60 +4,20 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon fa-book"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Reshelving Alexandria</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          Stay tuned!
         </p>
       </div>
     </div>
-    <nav>
-      <ul>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('intro')
-            }}
-          >
-            Intro
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('work')
-            }}
-          >
-            Work
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('about')
-            }}
-          >
-            About
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('contact')
-            }}
-          >
-            Contact
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <div className="logo">
+      <a href = "https://www.facebook.com/groups/1822973164691357"><i className="icon fa-facebook-square"></i></a>
+    </div>
+
   </header>
 )
 
