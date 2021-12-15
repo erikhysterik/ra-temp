3e4ce2547a02d0cc76d41d4fc83d5b4a52d9820a
Erik Sautter <erikhysterik@users.noreply.github.com>
2021-12-14 19:20:57 -0600

    fix

--- src/components/RedirectHome.js
+++ src/components/RedirectHome.js
@@ -3,7 +3,7 @@ import React from "react"
 class RedirectHome extends React.Component {
     componentDidMount() {
       console.log('Check 101')
-      typeof window !== "undefined" && window.gtag("event", "exception", { description: this.props.target })
+      typeof window !== "undefined" && window.gtag("event", "exception", { description: "404" })
       setTimeout(() => {if (typeof window !== 'undefined') window.location = '/'}, 1500)
     }
     render() {
