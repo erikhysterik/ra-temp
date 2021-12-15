3332c2be294cd73a353377fc0bbe05f03fbb8eb4
Erik Sautter <erikhysterik@users.noreply.github.com>
2021-12-14 21:52:15 -0600

    PDF updates

--- src/pages/2022-reading-challenge.js
+++ src/pages/2022-reading-challenge.js
@@ -7,12 +7,21 @@ const ChallengePage = () => (
   <Layout>
     <h1>2022 Reading Challenges</h1>
     <p>Available as a downloadable PDF</p>
+    <div>
+      <a href = {"/2022_Reading_Challenge.pdf"} target="_blank" rel="noopener noreferrer">Reading Challenge</a>
+    </div>
+    <div>
+      <a href = {"/2022_52_Reading_Challenge.pdf"} target="_blank" rel="noopener noreferrer">52 Book Reading Challenge</a>
+    </div>
+    <div>
+      <a href = {"/2022_Teen_Reading_Challenge.pdf"} target="_blank" rel="noopener noreferrer">Teen Reading Challenge</a>
+    </div>
+    <div>
+      <a href = {"/2022_Kids_Reading_Challenge.pdf"} target="_blank" rel="noopener noreferrer">Kids Reading Challenge</a>
+    </div>
     <p>
-      <a href = {"/2022_Reading_Challenges.pdf"} target="_blank" rel="noopener noreferrer">Download</a>
     </p>
-    <p>
-      <Link to="/">Back to Home</Link>
-    </p>
+    <Link to="/">Back to Home</Link>
   </Layout>
 )
 
