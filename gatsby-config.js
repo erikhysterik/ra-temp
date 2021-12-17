120c7fcfe1996597aa30f764007779d60b60abf1
Erik Sautter <erikhysterik@users.noreply.github.com>
2021-12-16 23:16:31 -0600

    update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#3e383e',
         theme_color: '#3e383e',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/RA.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
